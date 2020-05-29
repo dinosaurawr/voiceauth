9758a327aedb31dfaebf0af746dafae4de6ca679
Aleksandr Kryukov <dinosaurawr.1@gmail.com>
2020-05-29 16:51:38 +0300

    refactoring

--- VoiceFeatures.py
+++ VoiceFeatures.py
@@ -1,8 +1,83 @@
-
+import librosa
+import numpy
+import os
+import constants
+import operator
 
 class VoiceFeatures:
 
-    def __init__(self, label_for: str, mfcc: list):
-        self.mfcc = mfcc
-        self.label = label_for
+    def __init__(self, label='Unlabled', load_from_file=False, save_coeffs=False, series=[]):
+
+        self.label = label
+        self.coeffs_folder = os.path.join(os.getcwd(), 'mfcc_coeffs')
+        self.series = series
+        if (load_from_file):
+            self.mfcc = self.load_coefs_from_file_by_label()
+        else:
+            self.mfcc = self.compute_mfcc()
+            if (save_coeffs):
+                self.save_coefs()
+
+        
+    def compute_mfcc(self):
+        mfcc = librosa.feature.mfcc(
+            self.series,
+            sr=constants.SAMPLE_RATE,
+            n_mfcc=constants.MFCC_COUNT,
+            n_fft=2048
+        )
+        
+        mfcc = numpy.sum(mfcc[2:], axis=-1)
+        mfcc = mfcc / numpy.max(numpy.abs(mfcc))
+        return mfcc
+
+    def get_euclidian_distance_with_other(self, mfcc_vector):
+        return numpy.sum((self.mfcc - mfcc_vector) ** 2)
+
+    def save_coefs(self):
+        file = open(
+            os.path.join(
+                self.coeffs_folder,
+                f'{self.label}.txt'
+            ),
+            'w'
+        )
+        file.write(
+            ' '.join([str(coeff) for coeff in self.mfcc])
+        )
+        file.close()
+        return
+
+    def load_coefs_from_file_by_label(self):
+        file = open(
+            os.path.join(
+                self.coeffs_folder,
+                f'{self.label}.txt'
+            ),
+            'r'
+        )
+        cont = file.read()
+        self.mfcc = numpy.array(cont.split(' '), numpy.float32)
+        file.close()
+        return self.mfcc
+
+    def resolve_voice(self):
+        print(self.mfcc)
+        print(len(self.mfcc))
+        if len(self.mfcc) == 0:
+            return 'Not enough MFCC coeffs'
+        coefs_files = [
+            file for file in os.listdir(self.coeffs_folder)
+            if file.endswith('.txt')
+        ]
+        results = {}
+        for filename in coefs_files:
+            other_mfcc = numpy.array(
+                open(os.path.join(self.coeffs_folder, filename), 'r').read().split(' '),
+                dtype=numpy.float32)
+            euclidian = self.get_euclidian_distance_with_other(other_mfcc)
+            same_by = round(1 /euclidian * 100, 2)
+            print(f'{self.label} same with {filename.split(".")[0]} is {same_by}')
+            results[filename] = same_by
+        return max(results.items(), key=operator.itemgetter(1))[0]
         
--- VoiceResolver.py
+++ VoiceResolver.py
@@ -1,121 +1,14 @@
-import librosa
-import numpy
-from keras.models import Sequential
-from keras.models import load_model, save_model
-import os
-import operator
-from keras.layers import LSTM, Dense, Activation
-from keras.optimizers import Adam
-
 import constants
 from AudioManager import AudioManager
-
+from VoiceFeatures import VoiceFeatures
 
 class VoiceResolver:
     
-    def __init__(self):
-        self.audio_manager = AudioManager()
+    def __init__(self, resolver='mfcc'|'model'):
+        if (resolver == 'mfcc'):
+            self.resolver = 
         return
 
-    def resolve_voice(self, audio_series):
-        models_path = []
-        for (dirpath, dirnames, filenames) in os.walk(os.path.join(os.getcwd(), 'models')):
-            models_path.extend(filenames)
-            break
-        predictions = {}
-        for model_path in models_path:
-            model = load_model(os.path.join(os.getcwd(), 'models', model_path))
-            samples, _ = self.prepare_samples(audio_series, os.path.basename(model_path))
-            prediction = model.predict(samples)
-            predictions[model_path.split('.')[0]] = numpy.sum(prediction)
-        
-        print(predictions)
-        return max(predictions.items(), key=operator.itemgetter(1))[0]
-
-    def prepare_samples(self, audio_series, series_name, target=False):
-        print(f"Preparing samples for {series_name}")
-
-        data = librosa.stft(audio_series, n_fft=constants.MODEL_NFFT).swapaxes(0, 1)
-        samples = []
-
-        for i in range(0, len(data) - constants.BLOCK_LENGTH, constants.BLOCK_OVERLAP):
-            samples.append(numpy.abs(data[i:i + constants.BLOCK_LENGTH]))
-
-        results_shape = (len(samples), 1)
-        results = numpy.ones(results_shape) if target else numpy.zeros(results_shape)
-        return numpy.array(samples), results
     
-    def get_training_samples(self):
-        result = []
-        dirname, languages, _ = next(os.walk(os.path.join(os.getcwd(), 'samples')))
-        for language in languages:
-            _, _, records = next(os.walk(os.path.join(dirname, language)))
-            for record_path in records:
-                result.append((
-                    self.audio_manager.load_by_path(os.path.join(dirname, language, record_path)),
-                    record_path.split('.')[0]
-                    ))
-        return result
-        
-        
-    def train_for_new_voice(self, target_series, series_name):
-        training_series = []
-        print("Creating train data")
-        for series in target_series:
-            print(f"Adding {series_name} series to training set")
-            training_series.append((series, series_name, True))
-            
-        training_series.extend(self.get_training_samples())
-        
-        samples = [
-            ("japanese", "female", 1, 1),
-            ("japanese", "female", 1, 2),
-            ("japanese", "female", 1, 3),
-            ("japanese", "female", 1, 4),
-            ("french", "male", 1, 1),
-            ("french", "male", 1, 2),
-            ("french", "female", 1, 1),
-            ("french", "female", 1, 2),
-            ("french", "female", 1, 3),
-            ("english", "male", 1, 1),
-            ("english", "male", 2, 1),
-            ("english", "male", 2, 2),
-            ("english", "male", 2, 3),
-            ("russian", "male", 1, 1),
-            ("russian", "male", 1, 2)
-        ]
-        
-        X, Y = self.prepare_samples(*training_series[0])
-        for series in training_series[1:]:
-            dx, dy = self.prepare_samples(*series)
-            X = numpy.concatenate((X, dx), axis=0)
-            Y = numpy.concatenate((Y, dy), axis=0)
-            del dx, dy
-        
-        perm = numpy.random.permutation(len(X))
-        X = X[perm]
-        Y = Y[perm]
-        
-        if os.path.exists(os.path.join(os.getcwd(), 'models', f'{series_name}')):
-            model = load_model(os.path.join(os.getcwd(), 'models', f'{series_name}'))
-        else:
-            model = Sequential()
-            model.add(LSTM(128, return_sequences=True, input_shape=X.shape[1:]))
-            model.add(LSTM(64))
-            model.add(Dense(64))
-            model.add(Activation('tanh'))
-            model.add(Dense(16))
-            model.add(Activation('sigmoid'))
-            model.add(Dense(1))
-            model.add(Activation('hard_sigmoid'))
-        
-        model.compile(
-            Adam(lr=0.005), 
-            loss='binary_crossentropy', 
-            metrics=['accuracy'])
-        print(f"Training model for {series_name}")
-        model.fit(X, Y, epochs=50, batch_size=32, validation_split=0.2)
-        save_model(model,
-            os.path.join(os.getcwd(), "models", f"{series_name}.hd5"))
         
 
